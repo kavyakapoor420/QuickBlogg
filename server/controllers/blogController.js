4922e940871c078cf5f16d815bd77444e216324b
kavyakapoor420 <kavyakapoor412@gmail.com>
2026-03-04 22:26:42 +0530

    created get all blog and get blog by Id handler function

--- server/controllers/blogController.js
+++ server/controllers/blogController.js
@@ -44,3 +44,31 @@ export const addBlog=async(req,res)=>{
         console.log(err)
     }
 }
+
+export const getAllBlogs=async(req,res)=>{
+
+    try{
+         const blogs=await Blog.find({isPublished:true}).sort({createdAt:-1})
+
+         res.status(200).json({success:true,message:err.message})
+
+    }catch(err){
+         res.status(500).json({success:false,message:err.message})
+    }
+}
+
+export const getBlogById=async(req,res)=>{
+
+    try{
+         const  {blogId}=req.params ;
+         const blog=await Blog.findById(blogId)
+
+         if(!blog){
+            return res.status(404).json({success:false,message:'blog not found'})
+         }
+
+         res.status(200).json({success:true,blog})
+    }catch(err){
+         res.status(500).json({success:false,message:err.message})
+    }
+}
